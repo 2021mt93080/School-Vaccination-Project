c3d6d09e495654da0ee25445264cf147d9374e8e
warisdevkhan <warisk@imaginovation.net>
2022-10-30 13:50:14 +0530

    Updated Mobile App React Native

---
+++ MobileApplication/App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+   <WebView source={{ uri: 'http://103.47.47.49:8000/login' }} />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
